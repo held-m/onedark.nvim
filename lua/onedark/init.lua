8fc1b315e643d79679d5b680082b553bd5c53b86
Michael Held <git@mikeyheld.com>
2023-04-09 20:15:03 +0200

    my settings

--- lua/onedark/init.lua
+++ lua/onedark/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.styles_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }
+M.styles_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light', 'my' }
 
 ---Change onedark option (vim.g.onedark_config.option)
 ---It can't be changed directly by modifying that field due to a Neovim lua bug with global variables (onedark_config is a global variable)
